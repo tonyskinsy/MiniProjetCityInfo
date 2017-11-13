0ddcc32a5ebfd33bda7610f3c92c0feaa27961f2
Junior Noubissi <tonyskinsy@users.noreply.github.com>
2017-11-13 17:06:11 +0100

    Add files via upload

---
+++ CityInfo/cityServer.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+var express = require("express");
+var weather=require('./moduleTemperature');
+var resultats = [];
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+   res.sendFile(__dirname + '/public/infoBoard.html');
+});
+
+app.get('/findInformations', function (req, res) {
+    console.log("J'ai bien récupéré le nom de la ville : "+req.query.cityName);
+    weather.getCityInfoMeteo(req.query.cityName,function(body) {
+        console.log("Température de test : "+JSON.parse(body).main.temp+";");
+    res.end(JSON.stringify(body));
+  });
+
+  
+});
+
+app.listen(3000, function () {
+
+  console.log("Server is listening on port 3000");
+  console.log("Open http://localhost:3000 and upload some files!");
+
+});
---
+++ CityInfo/moduleTemperature.js
@@ -0,0 +1,36 @@
+//var city = "New+York";
+var http = require("http");
+
+function printMessage(city, temperature, pressure) {
+    console.log("A " + city + ", la température est de " + (temperature - 273.15) + "°C et la pression est de " + pressure + " pascals");
+}
+function getCityInfoMeteo(city, callback) {
+    var request = http.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=49495a854750288931f40fc3082d7c84", function (response) {
+        var body = "";
+        response.on('data', function (chunk) {
+            body += chunk;
+        });
+        response.on('end', function () {
+           // console.log("Elément retourné :"+body);
+            if (response.statusCode === 200) {
+                try {
+                    var data_weather = JSON.parse(body);
+                    printMessage(city, data_weather.main.temp, data_weather.main.pressure);
+                    callback(body);
+                } catch (error) {
+                    console.error(error.message);
+                }
+            }
+            else {
+                console.error("La ville saisie est inconnue. Veuillez vérifier votre saisie!")
+            }
+
+        });
+
+    });
+    request.on('error', function (error) {
+        console.error(error.message);
+    });
+}
+
+module.exports.getCityInfoMeteo = getCityInfoMeteo;
